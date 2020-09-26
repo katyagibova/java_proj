1d488071f75899687f946aace4bcdd7e07e8ef59
katyagibova <katyagibova@bk.ru>
2020-09-26 23:00:14 +0300

    java

---
+++ proj_6/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        Field field=new Field(in.nextInt());
+        field.Filling();
+        field.Output();
+        System.out.println("Result: "+field.searchResult());
+    }
+}
