2cf8142cdcc7e591e135752756e006c2467c9d67
katyagibova <katyagibova@bk.ru>
2020-10-11 15:00:14 +0300

    java

---
+++ proj_10/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MySwingApp app = new MySwingApp();
+            app.setVisible(true);
+        });
+    }
+}
